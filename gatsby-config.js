5e970c1af8c9180ca09aeadafa25ce6612fb7b9b
unknown <kishorenaskar1@gmail.com>
2021-12-23 12:15:19 +0530

    robots.txt added

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,10 +7,33 @@ require("dotenv").config({
   path: `.env.${process.env.NODE_ENV}`,
 })
 
+const {
+  NODE_ENV,
+  URL: NETLIFY_SITE_URL = "https://pratham-milan.netlify.app",
+  DEPLOY_PRIME_URL: NETLIFY_DEPLOY_URL = NETLIFY_SITE_URL,
+  CONTEXT: NETLIFY_ENV = NODE_ENV,
+} = process.env
+const isNetlifyProduction = NETLIFY_ENV === "production"
+const siteUrl = isNetlifyProduction ? NETLIFY_SITE_URL : NETLIFY_DEPLOY_URL
+
 module.exports = {
   /* Your site config here */
 
   siteMetadata: {
+    title: `Pratham Milan`,
+    description: `Pratham Milon is one of the most renowned wedding venues in Howrah, located on Domjur - Amta road, making it most accessible and convenient for all your guests.  `,
+    author: `@Kishore`,
+    twitterUsername: "@kishorenaskar2",
+    image: "/twitter-img.png",
+    keywords: [
+      "Pratham Milan Garden",
+      "Pratham Milan banquet hall",
+      "Banquet hall in Domjur",
+      "Banquet hall in Howrah",
+      "Marriage hall in Howrah",
+    ],
+    siteUrl,
+
     menu: [
       { name: "About", to: "/about" },
       { name: "Facilities", to: "/facilities" },
@@ -49,6 +72,53 @@ module.exports = {
           `privacy-page`,
           `404-page`,
         ],
+      },
+    },
+    `gatsby-plugin-sitemap`,
+    {
+      resolve: "gatsby-plugin-robots-txt",
+      options: {
+        resolveEnv: () => NETLIFY_ENV,
+        env: {
+          production: {
+            policy: [{ userAgent: "*" }],
+          },
+          "branch-deploy": {
+            policy: [{ userAgent: "*", disallow: ["/"] }],
+            sitemap: null,
+            host: null,
+          },
+          "deploy-preview": {
+            policy: [{ userAgent: "*", disallow: ["/"] }],
+            sitemap: null,
+            host: null,
+          },
+        },
+      },
+    },
+    {
+      resolve: `gatsby-plugin-nprogress`,
+      options: {
+        color: `lightgray`,
+        showSpinner: false,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `প্রথম মিলন`,
+        short_name: `প্রথম মিলন`,
+        start_url: `/`,
+        background_color: `#f7f0eb`,
+        theme_color: `#a2466c`,
+        display: `standalone`,
+        icon: `static/icon.png`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-offline`,
+      options: {
+        precachePages: [`/`],
       },
     },
   ],
